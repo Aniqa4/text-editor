8238287c3d065e435ee11a9a099772ceae68f483
Aniqa <aanika.chowdhury04@gmail.com>
2024-03-31 14:28:03 +0600

    layout

---
+++ app/components/Layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Layout() {
+  return (
+    <div>
+        <h1 className='text-3xl text-center py-5 bg-[#13bc2f47] font-semibold shadow-xl'>Text Editor</h1>
+    </div>
+  )
+}
+
+export default Layout
